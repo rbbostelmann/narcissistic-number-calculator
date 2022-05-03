8848ff2d5cd2377ca60f08d50816756bd39ad47f
rbbostelmann <rodrigobostelmann@gmail.com>
2022-05-03 13:51:14 -0300

    tried a different function

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ function narcissistic(value) {
     //Adds the individually pow-raised digits to one another and checks if narcissistic 
     return realDigits.reduce((previous, next) => previous + next) == value;
 }
+
+//Given a minmax number, calculates the narcissistic numbers from min up to max number
+function narc(min, max) {
+    //Empty array that will hold our collection or narcissistic numbers
+    let isit = [];
+    //Looping from min to max
+    for (let i = min; i <= max; i++) {
+        //Gets the lenght of every number
+        const len = Math.ceil(Math.log10(i + 1));
+
+        //Gets the digits then raises them to the power of the lenght of the initial number.
+        let digits = i.toString().split('');
+        let realDigits = digits.map(Number => Math.pow(Number, len))
+
+        //Adds the individually pow-raised digits to one another
+        let check = realDigits.reduce((previous, next) => previous + next);
+
+        //Checks and pushes values to the array
+        if (check == i) {
+            isit.push(i)
+        }
+    }
+    //Returns array of narcissistic numbers
+    return isit;
+}
+
+console.log(narc(1, 1741725))
